04dd20cd14667ed2627bd81d7eb984c6df220dd1
Tim Malstead <timmmalstead@gmail.com>
2020-04-15 13:07:30 -0700

    updates notes

--- src/App.js
+++ src/App.js
@@ -14,22 +14,23 @@ const SiteHeader = React.lazy(() =>
     new Promise((resolve) => setTimeout(resolve, 2000)),
   ]).then(([moduleExports]) => moduleExports)
 )
-//do a docker icon and... what else?
+//TECHS
+//do tech list, and add a docker icon and whatever else is needed.
+//and do a redux icon once you've done a little project with that
 
-//how to transition between components mounting?
-
-//nice bright grad as an option
-
-//for projects, hover over the pics and it will use a transition to size up the pic to make it bigger. probably do this with a transition
-
+//PROJECTS
+//hover over the pics and it will use a transition to size up the pic to make it bigger. probably do this with a transition in color and blending too
 //do all the projects in a loop with things going => and <= for rhythm
-
 //also bring in your app logos from your projects
 
+//THEMES
 //figure out what you want to have be dynamic in the theme. probably backgrounds,images,
-
 //colors of course and see if you can do dynamic favicons
+//nice bright grad as an option
 
+//PRINT, ILLO AND OTHER CREATIVE WORK
+
+//how to transition between components mounting?
 //attach meta og tags to html file before you are done
 const App = () => {
   const storage = window.localStorage
