18ddf97aa4a6d187956024296e296fd29fb13e66
Tim Malstead <timmmalstead@gmail.com>
2020-04-15 19:12:58 -0700

    finalizes tech icon styling

--- src/Techs/style.js
+++ src/Techs/style.js
@@ -6,7 +6,7 @@ const TechHolder = styled.div`
   width: 90%;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-evenly;
+  justify-content: center;
 
   @media (max-width: 800px) {
     width: 95%;
